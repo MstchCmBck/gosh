8d8a5696c2bab696d67508df9934ecd81cf6fe9a
mstchcmbk <damien.marguerite@pm.me>
2025-03-22 00:25:57 +0100

    Handling of the exit command

--- app/interpretor.go
+++ app/interpretor.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func run() {
@@ -15,6 +17,22 @@ func run() {
 		// Print default message
 		fmt.Println(unknownCommand(command))
 	}
+}
+
+func isExit(command string) (bool, int) {
+	// Will match "exit 123", not match "exit" or "exit123"
+	re := regexp.MustCompile(`^exit\s(\d+)`)	
+	// Early return if the command does not match the regex
+	if !re.Match([]byte(command)) {
+		return false, 0
+	}
+	// Extract the index from the command
+	index, err := strconv.Atoi(string(re.FindSubmatch([]byte(command))[1]))
+	// If the conversion fails, consider the command as not an exit command
+	if err != nil {
+		return false, 0
+	}
+	return true, index
 }
 
 func unknownCommand(command string) string {
--- app/interpretor_test.go
+++ app/interpretor_test.go
@@ -13,4 +13,42 @@ func TestUnknownCommand(t *testing.T) {
 	if want != unknownCommand(command) {
 		t.Errorf("unknownCommand(%q) = %q; want %q", command, unknownCommand(command), want)
 	}
-}	
+}
+
+func TestIsExit(t *testing.T) {
+	// Test with a command that is not an exit command
+	command := "exit"
+	want := false
+
+	if got, _ := isExit(command); got != want {
+		t.Errorf("isExit(%q) = %t; want %t", command, got, want)
+	}
+
+	// Test with a command that is an exit command
+	command = "exit123"
+	want = false
+
+	if got, _ := isExit(command); got != want {
+		t.Errorf("isExit(%q) = %t; want %t", command, got, want)
+	}
+
+	// Test with a command that is an exit command
+	command = "exit 0"
+	want = true
+	wantIndex := 0
+
+	if got, index := isExit(command); got != want && index != wantIndex {
+		t.Errorf("isExit(%q) = %t; want %t", command, got, want)
+		t.Errorf("isExit(%q) index = %d; wantIndex %d", command, index, wantIndex)
+	}
+
+	// Test with a command that is an exit command
+	command = "exit 123"
+	want = true
+	wantIndex = 123
+
+	if got, index := isExit(command); got != want && index != wantIndex {
+		t.Errorf("isExit(%q) = %t; want %t", command, got, want)
+		t.Errorf("isExit(%q) index = %d; wantIndex %d", command, index, wantIndex)
+	}
+}
