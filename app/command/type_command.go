e6a53ba57c4ba5fbe378936721e8cba69cbbf02b
mstchcmbk <damien.marguerite@pm.me>
2025-03-22 15:38:46 +0100

    Use exec.LookPath() over map creation at init

--- app/command/type_command.go
+++ app/command/type_command.go
@@ -2,21 +2,17 @@ package command
 
 import (
 	"fmt"
-	"os"
-	"strings"
+	"os/exec"
 )
 
 // TypeCommand string contains parameters send with the exit command
 // It implement the Execute method of the Command interface
 type TypeCommand string
 
-var execMap = make(map[string]string)
-
 func init() {
 	builtinCommands["type"] = func(params string) Command {
 		return TypeCommand(params)
 	}
-	execMap = getExecMap()
 }
 
 func (t TypeCommand) Execute() error {
@@ -26,33 +22,12 @@ func (t TypeCommand) Execute() error {
 		return nil
 	}
 
-	path, exists := execMap[string(t)]
-	if !exists {
+	path, err := exec.LookPath(string(t))
+	if err != nil {
 		fmt.Println(t + ": not found")
 		return nil
 	}
+
 	fmt.Println(string(t) + " is " + path + "/" + string(t))
 	return nil
-}
-
-func getExecMap() map[string]string {
-	path, exists := os.LookupEnv("PATH")
-	if !exists {
-		return nil
-	}
-	splitted := strings.Split(path, ":")
-
-	var emap = make(map[string]string)
-
-	for _, dir := range splitted {
-		files, err := os.ReadDir(dir)
-		if err != nil {
-			continue
-		}
-		for _, file := range files {
-			emap[file.Name()] = dir
-		}
-	}
-
-	return emap
 }
