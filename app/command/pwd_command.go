f5272907a795cd6f3f3e4c3094731eded54283ee
mstchcmbk <damien.marguerite@pm.me>
2025-03-22 16:38:16 +0100

    Add pwd built-in command

---
+++ app/command/pwd_command.go
@@ -0,0 +1,24 @@
+package command
+
+import "os"
+
+// Each new Command must implement the following functions:
+// - init() function to register the command in the builtinCommands map
+// - Execute() method to execute the command
+
+func init() {
+	builtinCommands["pwd"] = func(params string) Command {
+		return PwdCommand(params)
+	}
+}
+
+type PwdCommand string
+
+func (p PwdCommand) Execute() error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	println(wd)
+	return nil
+}
