e39e6822612195898cd9df4c908e26e3b1a4791c
mstchcmbk <damien.marguerite@pm.me>
2025-03-23 17:19:10 +0100

    Add stderr handling for built-in functions

--- app/command/redirection.go
+++ app/command/redirection.go
@@ -16,23 +16,29 @@ const (
 )
 
 // This function is used by all the built-in function to print their result
-func print(output string, cmd commandline) {
+func print(input string, cmd commandline) {
 	switch cmd.redirection {
-	case noredirection:
-		fmt.Print(output)
 	case stdout:
-		file, err := os.OpenFile(cmd.filepath, os.O_RDWR|os.O_CREATE, 0644)
-		if err != nil {
-			fmt.Println(err)
-		}
+	case stderr:
+		printFile(input, cmd.filepath)
+	case noredirection:
+	default:
+		fmt.Print(input)
+	}
+}
 
-		_, err = file.WriteString(output)
-		if err != nil {
-			fmt.Println(err)
-		}
+func printFile(input string, filename string) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-		if err = file.Close(); err != nil {
-			fmt.Println(err)
-		}
+	_, err = file.WriteString(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if err = file.Close(); err != nil {
+		fmt.Println(err)
 	}
 }
